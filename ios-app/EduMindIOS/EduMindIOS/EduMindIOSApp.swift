ee16b9b657a09303c1f28af599019947f818ce1f
yuan <2702965216@qq.com>
2026-03-13 09:10:57 +0800

    chore: track ios-app/EduMindIOS files in main repo

---
+++ ios-app/EduMindIOS/EduMindIOS/EduMindIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduMindIOSApp.swift
+//  EduMindIOS
+//
+//  Created by yuan on 2026/3/12.
+//
+
+import SwiftUI
+
+@main
+struct EduMindIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
